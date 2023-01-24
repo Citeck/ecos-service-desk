73e56de717aad020982a4213a029920b9e074dea
Roman.Makarskiy <romanchabest55@gmail.com>
2023-01-24 15:46:33 +0300

    Revert "SD-13 - SLA"
    This reverts commit 65e547c0c6ce7bc1d5d4344ac515aeab56b6b71e.

--- src/main/java/ru/citeck/ecos/webapp/servicedesk/domain/sla/api/records/SdSlaRecords.kt
+++ src/main/java/ru/citeck/ecos/webapp/servicedesk/domain/sla/api/records/SdSlaRecords.kt
@@ -184,7 +184,7 @@ class SdSlaRecords(
                         val slaDurations = slaParametersProvider.get(client, priority)
 
                         val sla2Duration = slaDurations.timeResolve
-                        val spentDuration = sla2SpentTime!!.toDuration(DurationUnit.MILLISECONDS)
+                        val spentDuration = sla2SpentTime.toDuration(DurationUnit.MILLISECONDS)
 
                         val remainingDuration = sla2Duration - spentDuration
 
@@ -208,30 +208,30 @@ class SdSlaRecords(
         }
     }
 
-    class RequestInfo(
-        var client: EntityRef = EntityRef.EMPTY,
-        var priority: String? = null,
+    data class RequestInfo(
+        val client: EntityRef = EntityRef.EMPTY,
+        val priority: String? = null,
 
         @AttName("sla_1_state")
-        var sla1State: SlaState? = null,
+        val sla1State: SlaState? = null,
 
         @AttName("sla_2_state")
-        var sla2State: SlaState? = null,
+        val sla2State: SlaState? = null,
 
         @AttName("firstReactionTimestamp")
-        var sla1CompletedAt: Instant? = null,
+        val sla1CompletedAt: Instant? = null,
 
         @AttName("sla_1_due_date")
-        var sla1DueDate: Instant? = null,
+        val sla1DueDate: Instant? = null,
 
         @AttName("closedTimestamp")
-        var sla2CompletedAt: Instant? = null,
+        val sla2CompletedAt: Instant? = null,
 
         @AttName("sla_2_spent_time")
-        var sla2SpentTime: Long? = null,
+        val sla2SpentTime: Long? = null,
 
         @AttName("sla_2_due_date")
-        var sla2DueDate: Instant? = null
+        val sla2DueDate: Instant? = null
     )
 
     private data class SlaRecord(
