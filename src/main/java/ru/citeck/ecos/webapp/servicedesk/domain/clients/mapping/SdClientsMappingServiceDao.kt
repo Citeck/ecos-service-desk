fc915a0b2ab3c4e7d02c64efde2c79b86d0542bb
Pavel Simonov <pavel.simonov@citeck.ru>
2026-04-17 03:22:31 +0700

    COREDEV-130 - Add clients mapping cache. Release 1.19.4

---
+++ src/main/java/ru/citeck/ecos/webapp/servicedesk/domain/clients/mapping/SdClientsMappingServiceDao.kt
@@ -0,0 +1,37 @@
+package ru.citeck.ecos.webapp.servicedesk.domain.clients.mapping
+
+import org.springframework.stereotype.Component
+import ru.citeck.ecos.records3.record.dao.AbstractRecordsDao
+import ru.citeck.ecos.records3.record.dao.query.RecordsQueryDao
+import ru.citeck.ecos.records3.record.dao.query.dto.query.RecordsQuery
+import ru.citeck.ecos.webapp.api.entity.EntityRef
+
+@Component
+class SdClientsMappingServiceDao(
+    private val cache: SdClientsMappingRefCache
+) : RecordsQueryDao, AbstractRecordsDao() {
+
+    override fun queryRecords(recsQuery: RecordsQuery): Any? {
+        if (recsQuery.language == MappingByClientQuery.LANG) {
+            val query = recsQuery.getQuery(MappingByClientQuery::class.java)
+            return MappingByClientQuery.Resp(cache.getMappingRef(query.client))
+        }
+        return null
+    }
+
+    override fun getId(): String {
+        return "clients-mapping-service"
+    }
+
+    data class MappingByClientQuery(
+        val client: EntityRef
+    ) {
+        companion object {
+            const val LANG = "mapping-by-client"
+        }
+
+        data class Resp(
+            val mappingRef: EntityRef
+        )
+    }
+}
